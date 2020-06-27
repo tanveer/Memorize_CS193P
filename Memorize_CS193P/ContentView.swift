85c3d63ad6f50928ee23168a4e7110ab715d2b9f
Tanveer Bashir <tan.m.bashir@gmail.com>
2020-06-27 14:45:03 -0400

    Initial Commit

---
+++ Memorize_CS193P/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize_CS193P
+//
+//  Created by Tanveer Bashir on 6/27/20.
+//  Copyright © 2020 Tanveer Bashir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
